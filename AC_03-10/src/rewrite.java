ca776059caba66d76505a4dc1aece8e2a4531398
MadelynTav <Madelyntav@gmail.com>
2015-03-12 14:32:42 -0400

    Group1 additions

---
+++ AC_03-10/src/rewrite.java
@@ -0,0 +1,13 @@
+/**
+// * Created by c4q-madelyntavarez on 3/10/15.
+// */
+//import java.util.Scanner;
+//public class rewrite {
+//    public static void main(int[] args){
+//        Scanner scanner= new Scanner (System.in);
+//
+//        for (int i=0; i<=100; i++ );
+//
+//        System.out.println(i);
+//    }
+//}
